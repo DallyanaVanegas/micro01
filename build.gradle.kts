30887f5e3ff7fe0e1d5c739ac40f140ecc197c13
DallyanaVanega <davanegas@uce.edu.ec>
2023-06-21 12:46:30 -0500

    servidor de aplicaciones

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,10 @@ dependencies {
 
     implementation("io.quarkus:quarkus-jdbc-postgresql")
     implementation("io.quarkus:quarkus-hibernate-orm-panache")
+
+    implementation("io.quarkus:quarkus-config-yaml")
+
+
 }
 
 java{
--- src/main/java/com/distribuida/rest/PersonaRest.java
+++ src/main/java/com/distribuida/rest/PersonaRest.java
@@ -1,56 +1,33 @@
-package com.distribuida;
+package com.distribuida.rest;
 
 import com.distribuida.db.Persona;
-import com.distribuida.rest.TestConfiguracion;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
-import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 import java.util.List;
+@Path("/Personas")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+@Transactional
+public class PersonaRest {
 
-@Path("/Persona")
-public class GreetingResource {
-
-    class Message {
-        private String msg = "hola mundo";
-
-        public String getMsg() {
-            return msg;
-        }
-
-        public void setMsg(String msg) {
-            this.msg = msg;
-        }
-
-
-    }
-
-  /*  @GET
-    @Path("")
-    @Produces(MediaType.APPLICATION_JSON)
-    @Transactional
-    //public Message hello() {
-      //  return new Message();
-   // }
+    @GET
+    /*public Message hello() {
+        return new Message();
+    }*/
     public List<Persona> listarPersonas() {
         return Persona.listAll();
     }
 
     @GET
-    @Path("/id")
-    @Produces(MediaType.APPLICATION_JSON)
-    @Transactional
+    @Path("/{id}")
     public Persona personaId(@PathParam("id") Long id) {
         Persona persona = Persona.findById(id);
         return persona;
     }
     @POST
-    @Path("/crear")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    @Transactional
     public Response crearPersona(Persona persona) {
         persona.persist();
 
@@ -60,10 +37,7 @@ public class GreetingResource {
     }
 
     @PUT
-    @Path("/actualizar/{id}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    @Transactional
+    @Path("/{id}")
     public Response actualizarPersona(@PathParam("id") Long id, Persona updatedPersona) {
         Persona persona = Persona.findById(id);
 
@@ -78,9 +52,7 @@ public class GreetingResource {
     }
 
     @DELETE
-    @Path("/borrar/{id}")
-    @Produces(MediaType.APPLICATION_JSON)
-    @Transactional
+    @Path("/{id}")
     public Response borrarPersona(@PathParam("id") Long id) {
         Persona persona = Persona.findById(id);
         if (persona != null) {
@@ -90,5 +62,5 @@ public class GreetingResource {
         } else {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
-    }*/
+    }
 }
